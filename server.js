b04686c92a19d52f4560aad1030a53e90fbe1766
slipskid <2773262@gmail.com>
2019-12-19 11:27:58 -0500

    Initial commit from boilerplate

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
-'use strict';
+const cors = require('cors')
+const express = require('express')
+const multer = require('multer')
 
-var express = require('express');
-var cors = require('cors');
+const app = express()
+const upload = multer({ dest: 'uploads/' })
 
-// require and use "multer"...
+app.use(cors())
+app.use(express.static('public'))
 
-var app = express();
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+})
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
-});
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
-});
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log('Server listening on port:', server.address().port)
+})
